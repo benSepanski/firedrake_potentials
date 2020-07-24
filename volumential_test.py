0f9ea2714936fff0fd65deb93ba48d4708fd5827
benSepanski <ben_sepanski@alumni.baylor.edu>
2020-07-24 10:09:00 -0500

    Started using an actual kernel for this example

--- volumential_test.py
+++ volumential_test.py
@@ -24,39 +24,26 @@ def main():
     V = FunctionSpace(m, 'CG', 1)
     f = Function(V).interpolate(xx[0] * xx[1])
 
-    # build sumpy kernel for sigma * e^{-r/alpha}
-    logger.info("Setting up sumpy")
-    sigma = 1.0
-    alpha = 1.0
-    dim = m.geometric_dimension()
-
-    from math import e
-    from pymbolic.primitives import make_sym_vector, Power
-    from sumpy.symbolic import pymbolic_real_norm_2
-    r = pymbolic_real_norm_2(make_sym_vector("d", dim))
-    expr = Power(e, -r / alpha)
-
-    from sumpy.kernel import ExpressionKernel
-    kernel = ExpressionKernel(dim,
-                              expr,
-                              global_scaling_const=sigma,
-                              is_complex_valued=False)
+    from sumpy.kernel import LaplaceKernel
+    kernel = LaplaceKernel(m.geometric_dimension())
 
     # Build VolumePotential external operator
     cl_ctx = cl.create_some_context()
     queue = cl.CommandQueue(cl_ctx)
     potential_data = {
         'kernel': kernel,
-        'kernel_type': "ScaledGaussian",
+        'kernel_type': "Laplace",
         'cl_ctx': cl_ctx,
         'queue': queue,
         'nlevels': 5,
         'm_order': 10,
-        'dataset_filename': "gaussianpotential.hdf5",
+        'dataset_filename': "laplace.hdf5",
+        'root_extent': 2,
         'table_kwargs': {
+            'force_recompute': True,
             'n_brick_quad_points': 120,
             'adaptive_level': False,
-            'use_symmetry': True,
+            'use_symmetry': False,
             'alpha': 0,
             'n_levels': 1,
             },
