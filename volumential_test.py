678e75a6b57534cce5ce9b3ed57a807be20d0a80
benSepanski <ben_sepanski@alumni.baylor.edu>
2020-08-10 12:37:40 -0500

    Added 3D option

--- volumential_test.py
+++ volumential_test.py
@@ -3,7 +3,7 @@ import numpy as np
 import pyopencl as cl
 
 from firedrake import (
-    UnitSquareMesh, SpatialCoordinate, as_tensor,
+    UnitSquareMesh, SpatialCoordinate, UnitCubeMesh, as_tensor,
     exp, FunctionSpace, Function,
     VolumePotential, sqrt, assemble, inner, dx)
 
@@ -19,20 +19,28 @@ else:
 
 def main():
     # make function space and function
-    logger.info("Building function space")
-    m = UnitSquareMesh(32, 32)
+    dim = 3  # 2 or 3
+    order = 1
+
+    logger.info(f"building {dim}D source and solution exprs")
+    if dim == 2:
+        m = UnitSquareMesh(32, 32)
+    elif dim == 3:
+        m = UnitCubeMesh(32, 32, 32)
+    else:
+        raise ValueError("dim must be 2 or 3, not %s" % dim)
     # get spatial coordinate, shifted so that [0,1]^2 -> [-0.5,0.5]^2
     xx = SpatialCoordinate(m)
-    shifted_xx = as_tensor([xx[0] - 0.5, xx[1] - 0.5])
-
+    shifted_xx = as_tensor([xx_i - 0.5 for xx_i in xx])
+    norm2 = sum([xx_i * xx_i for xx_i in shifted_xx])
     alpha = 160
-    norm2 = shifted_xx[0] * shifted_xx[0] + shifted_xx[1] * shifted_xx[1]
-    source_expr = -(4 * alpha ** 2 * norm2 - 4 * alpha) * exp(-alpha * norm2)
+    source_expr = -(4 * alpha ** 2 * norm2 - 2 * dim * alpha) * exp(
+        -alpha * norm2)
     sol_expr = exp(-alpha * norm2)
+
     logger.info("source_expr : %s" % source_expr)
     logger.info("sol_expr : %s" % sol_expr)
 
-    order = 4
     logger.info(f"Building FunctionSpace of order {order}")
     fspace = FunctionSpace(m, 'CG', order)
     logger.info("interpolating source and solution")
@@ -60,7 +68,7 @@ def main():
         'queue': queue,
         'nlevels': 6,
         'm_order': 20,
-        'dataset_filename': f"laplace-order{order}.hdf5",
+        'dataset_filename': f"laplace-order{order}-{dim}D.hdf5",
         'grp_factory': grp_factory,
         'root_extent': 2,
         'table_compute_method': "DrosteSum",
@@ -80,10 +88,10 @@ def main():
 
     logger.info("Evaluating potential and assembling L^2 Error")
     ell2_difference = sqrt(assemble(inner(pot - sol, pot - sol) * dx))
-    print("L^2 difference: %e" % ell2_difference)
 
     logger.info("Evaluating potential and testing projection DG->CG space")
     pot.evaluate(continuity_tolerance=1e-8)
+    print("L^2 difference: %e" % ell2_difference)
 
 
 if __name__ == '__main__':
